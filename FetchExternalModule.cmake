0544a1a0069be4bf8b3a671454eaa9e9320a5acf
Dimo Markov <dimomarkov@gmail.com>
2023-08-12 09:42:31 +0300

    Modernized cmake; Added some sample code for tests (WIP)

---
+++ FetchExternalModule.cmake
@@ -0,0 +1,22 @@
+include(FetchContent)
+
+function(fetch_external_module NAME GIT_REPOSITORY REPO GIT_TAG TAG)
+    if(NOT DEFINED LANGULUS_EXTERNAL_DIRECTORY)
+        message(FATAL_ERROR "LANGULUS_EXTERNAL_DIRECTORY is not defined")
+    endif()
+
+    message(STATUS "Fetching external library ${NAME}...")
+    FetchContent_Declare(
+        ${NAME}
+        GIT_REPOSITORY  ${REPO}
+        GIT_TAG         ${TAG}
+        SOURCE_DIR      "${LANGULUS_EXTERNAL_DIRECTORY}/${NAME}-src"
+        SUBBUILD_DIR    "${LANGULUS_EXTERNAL_DIRECTORY}/${NAME}-subbuild"
+        ${ARGN}
+    )
+    FetchContent_MakeAvailable(${NAME})
+	
+	string(TOLOWER ${NAME} LOWERCASE_NAME)
+    set(${NAME}_SOURCE_DIR "${${LOWERCASE_NAME}_SOURCE_DIR}" CACHE INTERNAL "")
+    set(${NAME}_BINARY_DIR "${${LOWERCASE_NAME}_BINARY_DIR}" CACHE INTERNAL "")
+endfunction()
